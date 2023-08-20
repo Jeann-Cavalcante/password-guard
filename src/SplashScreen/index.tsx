3465705a51600739ea6887c5d948df920a03554d
Jean <jeanncavalcante@gmail.com>
2023-08-19 22:48:31 -0300

    feat: create pags onboarding

---
+++ src/SplashScreen/index.tsx
@@ -0,0 +1,22 @@
+import { View } from "react-native";
+import LottieView from "lottie-react-native";
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <LottieView
+        source={require("./assets/pass.json")}
+        loop
+        autoPlay
+        resizeMode="contain"
+        style={{
+          width: scale(800),
+          height: verticalScale(800),
+        }}
+      />
+    </View>
+  );
+};
+
+export default SplashScreen;
