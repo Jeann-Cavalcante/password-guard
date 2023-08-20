6a2a28e9c5b01e9745926e06cfcadb01f0251d9a
Jean <jeanncavalcante@gmail.com>
2023-08-19 22:08:26 -0300

    commit initial

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LottieView from "lottie-react-native";
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <LottieView
+        source={require("./assets/pass.json")}
+        loop
+        autoPlay
+        resizeMode="contain"
+        style={{
+          width: scale(800),
+          height: verticalScale(800),
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
